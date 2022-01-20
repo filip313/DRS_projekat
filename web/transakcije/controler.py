aebe2e654b2f66373858530e9aa993cdbe35d7f2
filip313 <filip.ignjatovic12@gmail.com>
2022-01-20 20:16:04 +0100

    adsf

--- web/transakcije/controler.py
+++ web/transakcije/controler.py
@@ -1,5 +1,7 @@
+from nis import cat
+from re import L
 from transakcije import *
-from transakcije.forme import UplataForm
+from transakcije.forme import UplataForm, PrenosForm
 from urllib import request as req
 from urllib.error import HTTPError
 import json
@@ -35,3 +37,52 @@ def uplata():
             return render_template("uplata.html",form=form)
     else:
         return redirect(url_for("user.login"))
+
+
+@transakcije.route('/prenos', methods=['GET', 'POST'])
+def prenos():
+    if 'user' in session:
+        form = PrenosForm()
+        izbori = []
+        for s in session['user']['stanja']:
+            izbori.append((s['valuta'],s['valuta']))
+        form.valuta.choices = izbori
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                data={"posiljalac":session['user']['email'], "primalac":form.primalac.data, "valuta":form.valuta.data, "iznos":form.iznos.data}
+                data = jsonify(data).get_data()
+                zahtev = req.Request("http://localhost:5000/prenos")
+                zahtev.add_header('Content-Type', 'application/json; charset=utf-8')
+                zahtev.add_header('Content-Length', len(data))
+                try:
+                    ret = req.urlopen(zahtev, data)
+                    flash(json.loads(ret.read()), category='primary')
+                    return redirect(url_for('index'))
+                except HTTPError as e:
+                    flash(e.read().decode(), category='danger')
+                    return render_template('prenos.html', form=form)
+            
+            if form.errors != {}:
+                for msg in form.errors.values():
+                    flash(msg.pop(), category='danger')
+                return render_template('prenos.html', form=form)
+        else:
+            return render_template('prenos.html', form=form)
+    else:
+        redirect(url_for('user.login'))
+
+    
+@transakcije.route('/prikaz', methods=['GET'])
+def prikaz_transakcija():
+    if 'user' in session:
+        try:
+            ret = req.urlopen(f'http://localhost:5000/transakcije/{session["user"]["id"]}')
+            user = json.loads(ret.read())
+            session['user'] = user
+            return render_template('transakcije.html', poslate=user['poslate_transakcije'], primljene=user['primljene_transakcije'])
+        except HTTPError as e:
+            flash(e.read().decode(), category='danger')
+            return redirect(url_for('index'))
+
+    else:
+        return redirect(url_for('user.login'))
--- web/transakcije/forme.py
+++ web/transakcije/forme.py
@@ -1,9 +1,15 @@
 from dataclasses import field
 from user.forme import KarticaForm
-from wtforms import StringField, PasswordField, SubmitField,IntegerField
+from wtforms import StringField, PasswordField, SubmitField,IntegerField, SelectField, FloatField 
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from flask_wtf import FlaskForm
 
 class UplataForm(KarticaForm):
     stanje=IntegerField(label='Iznos:  ', validators=[DataRequired()])
     submit = SubmitField(label='Uplata')
+
+class PrenosForm(FlaskForm):
+    primalac = StringField(label="Primalac: ", validators=[Email("Email nije ispravan!"), DataRequired()])
+    valuta = SelectField(label="Valuta: ", validators=[DataRequired()])
+    iznos = FloatField(label="Iznos: ", validators=[DataRequired()])
+    submit = SubmitField(label="Posalji korisniku")
